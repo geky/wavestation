954b2b47e2fcf6169c8ab8c947430c58669fe6e0
Christopher Haster <geky@geky.net>
2023-05-02 15:11:06 -0500

    More stuff: xorshift64, reproducible seeds, Poisson hallways

--- wavestation.py
+++ wavestation.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-# TODO allow seed argument
-# TODO just use xorshift?
-
 import random
 import itertools as it
 import math
@@ -17,56 +14,102 @@ class Bubble:
     def __repr__(self):
         return repr((self.x, self.y, self.r))
 
-def main(size, width, height, expand_prob, expand_off):
-    def randsize():
-        bsize = 1
-        while True:
-            if not (random.random() < expand_prob):
-                break
-            bsize +=1
-        return bsize
+class Xorshift64:
+    def __init__(self, seed=None):
+        if seed is None:
+            seed = random.randint(1, 0xffffffffffffffff)
+        self.seed = seed or 1
+        self.state = self.seed
 
-    def dirx(dir):
-        if dir == 1:
-            return +1
-        elif dir == 3:
-            return -1
-        else:
-            return 0
+    def next(self):
+        x = self.state
+        x ^= (x << 13) & 0xffffffffffffffff
+        x ^= (x >>  7) & 0xffffffffffffffff
+        x ^= (x << 17) & 0xffffffffffffffff
+        self.state = x
+        return x/0x10000000000000000
 
-    def diry(dir):
-        if dir == 0:
-            return +1
-        elif dir == 2:
-            return -1
-        else:
-            return 0
+    def range(self, start, stop=None, step=1):
+        if stop is None:
+            start, stop = 0, start
+        size = (stop-start)//step
+        i = math.floor(self.next() * size)
+        return (i*step) + start
 
-    def dist(a, b):
-        xd = a[0]-b[0]
-        yd = a[1]-b[1]
-        return math.sqrt(xd*xd + yd*yd)
+    def bool(self, p):
+        return self.next() < p
+
+    def poisson(self, p):
+        count = 0
+        while self.bool(p):
+            count += 1
+        return count
+
+    def __iter__(self):
+        return self.next()
+
+
+def dirx(dir):
+    if dir == 1:
+        return +1
+    elif dir == 3:
+        return -1
+    else:
+        return 0
+
+def diry(dir):
+    if dir == 0:
+        return +1
+    elif dir == 2:
+        return -1
+    else:
+        return 0
+
+def distsq(a, b):
+    return (a[0]-b[0])**2 + (a[1]-b[1])**2
+    
+def dist(a, b):
+    return math.sqrt(distsq(a, b))
+
+
+def main(size, *,
+        width=80,
+        height=40,
+        seed=None,
+        bubble_p=0.5,
+        extend_p=0.5,
+        small_map=False,
+        small_width=8,
+        small_height=8,
+        bubble_map=False):
+
+    # initialize with either provided seed or actually random seed
+    prng = Xorshift64(seed)
+    print('seed: 0x%016x' % prng.seed)
 
     # initialize our first bubble with a random size
-    bubbles = [Bubble(0,0,randsize(),None)]
+    bubbles = [Bubble(0, 0, 1+prng.poisson(bubble_p), None)]
     used = bubbles[0].r
 
     while used < size:
         # choose a bubble
-        parent = bubbles[random.randrange(len(bubbles))]
+        parent = bubbles[prng.range(len(bubbles))]
         # choose a direction
-        dir = random.randrange(4)
+        dir = prng.range(4)
         # choose a size
-        r = randsize()
+        r = 1+prng.poisson(bubble_p)
 
         # calculate new position
-        x = parent.x+dirx(dir)*(parent.r+r+expand_off)
-        y = parent.y+diry(dir)*(parent.r+r+expand_off)
+        x = parent.x+dirx(dir)*(parent.r+r+1+prng.poisson(extend_p))
+        y = parent.y+diry(dir)*(parent.r+r+1+prng.poisson(extend_p))
 
         # but wait, is there a collision?
         collision = False
         for bubble in bubbles:
-            if dist((x,y), (bubble.x, bubble.y)) <= r+bubble.r:
+            if bubble is parent:
+                continue
+            # check bubble collision
+            if distsq((x,y), (bubble.x, bubble.y)) <= (r+bubble.r)**2:
                 collision = True
                 break
         if collision:
@@ -76,11 +119,65 @@ def main(size, width, height, expand_prob, expand_off):
         bubbles.append(Bubble(x, y, r, parent))
         used += bubbles[-1].r
 
-    map = {(x,y): ' ' for x, y in it.product(range(width), range(height))}
+    # find bounds
+    lower, upper = (0, 0), (1, 1)
+    for bubble in bubbles:
+        lower = (min(lower[0], bubble.x-bubble.r),
+                min(lower[1], bubble.y-bubble.r))
+        upper = (max(upper[0], bubble.x+bubble.r),
+                max(upper[1], bubble.y+bubble.r))
+    orig = lower
+    width = upper[0]-lower[0]
+    height = upper[1]-lower[1]
+    print('widthxheight: %dx%d' % (width, height))
+
+    # shift all bubbles into range 0,0 -> width,height
+    for bubble in bubbles:
+        bubble.x -= orig[0]
+        bubble.y -= orig[1]
+
+    # show small map?
+    if small_map:
+        scale = (small_width/width, small_height/height)
+        smap = {(x,y): ' ' for x, y in it.product(
+                range(small_width), range(small_height))}
+        for bubble in bubbles:
+            if bubble.parent is None:
+                continue
+            if bubble.x != bubble.parent.x:
+                for x in range(
+                        min(math.floor(bubble.x*scale[0]),
+                            math.floor(bubble.parent.x*scale[0])),
+                        max(math.floor(bubble.x*scale[0]),
+                            math.floor(bubble.parent.x*scale[0]))):
+                    if smap.get((x, math.floor(bubble.y*scale[1]))) == '|':
+                        smap[(x, math.floor(bubble.y*scale[1]))] = '+'
+                    else:
+                        smap[(x, math.floor(bubble.y*scale[1]))] = '-'
+            if bubble.y != bubble.parent.y:
+                for y in range(
+                        min(math.floor(bubble.y*scale[1]),
+                            math.floor(bubble.parent.y*scale[1])),
+                        max(math.floor(bubble.y*scale[1]),
+                            math.floor(bubble.parent.y*scale[1]))):
+                    if smap.get((math.floor(bubble.x*scale[0]), y)) == '-':
+                        smap[(math.floor(bubble.x*scale[0]), y)] = '+'
+                    else:
+                        smap[(math.floor(bubble.x*scale[0]), y)] = '|'
+        for bubble in bubbles:
+            smap[(
+                math.floor((bubble.x)*scale[0]),
+                math.floor((bubble.y)*scale[1]))] = 'o'
+
+        for y in range(small_height):
+            print(''.join(smap[(x,y)] for x in range(small_width)))
+
+    # render bubble map
+    bmap = {(x,y): ' ' for x, y in it.product(range(width), range(height))}
     for bubble in bubbles:
         for x, y in it.product(range(width), range(height)):
-            if dist((x,y), (bubble.x+width//2,bubble.y+height//2)) <= bubble.r:
-                map[(x,y)] = '.'
+            if dist((x,y), (bubble.x,bubble.y)) <= bubble.r:
+                bmap[(x,y)] = '.'
     for bubble in bubbles:
         if bubble.parent is None:
             continue
@@ -88,17 +185,25 @@ def main(size, width, height, expand_prob, expand_off):
             for x in range(
                     min(bubble.x, bubble.parent.x),
                     max(bubble.x, bubble.parent.x)):
-                map[(x+width//2, bubble.y+height//2)] = '-'
+                if bmap.get((x, bubble.y)) == '|':
+                    bmap[(x, bubble.y)] = '+'
+                else:
+                    bmap[(x, bubble.y)] = '-'
         if bubble.y != bubble.parent.y:
             for y in range(
                     min(bubble.y, bubble.parent.y),
                     max(bubble.y, bubble.parent.y)):
-                map[(bubble.x+width//2, y+height//2)] = '|'
+                if bmap.get((bubble.x, y)) == '-':
+                    bmap[(bubble.x, y)] = '+'
+                else:
+                    bmap[(bubble.x, y)] = '|'
     for bubble in bubbles:
-        map[(bubble.x+width//2, bubble.y+height//2)] = 'o'
+        bmap[(bubble.x, bubble.y)] = 'o'
 
-    for y in range(height):
-        print(''.join(map[(x,y)] for x in range(width)))
+    # show bubble map?
+    if bubble_map:
+        for y in range(height):
+            print(''.join(bmap[(x,y)] for x in range(width)))
 
 
 if __name__ == "__main__":
@@ -112,26 +217,43 @@ if __name__ == "__main__":
         type=lambda x: int(x, 0),
         help="Size.")
     parser.add_argument(
+        '--seed',
+        type=lambda x: int(x, 0),
+        default=None,
+        help="Seed.")
+    parser.add_argument(
         '-W', '--width',
         type=lambda x: int(x, 0),
-        default=40,
         help="Render width.")
     parser.add_argument(
         '-H', '--height',
         type=lambda x: int(x, 0),
-        default=20,
         help="Render height.")
     parser.add_argument(
-        '--expand-prob',
+        '--bubble-p',
         type=float,
-        default=0.5,
-        help="Probability to expand capsule.")
+        help="Probability to expand a bubble.")
     parser.add_argument(
-        '--expand-off',
+        '--extend-p',
+        type=float,
+        help="Probability to extend a hallway.")
+    parser.add_argument(
+        '-s', '--small-map', '--small',
+        action='store_true',
+        help="Show small map.")
+    parser.add_argument(
+        '--small-width',
         type=lambda x: int(x, 0),
-        default=1,
-        help="Distance from capsule new capsules are created.")
-
+        help="Width of small map.")
+    parser.add_argument(
+        '--small-height',
+        type=lambda x: int(x, 0),
+        help="Height of small map.")
+    parser.add_argument(
+        '-b', '--bubble-map', '--bubble',
+        action='store_true',
+        help="Show bubble map.")
+        
     sys.exit(main(**{k: v
         for k, v in vars(parser.parse_args()).items()
         if v is not None}))
